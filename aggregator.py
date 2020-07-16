7f2bcadac76ae2f610a85cec73387454b76dd33f
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-07-16 08:54:42 +0300

    Fix build and output format

--- aggregator.py
+++ aggregator.py
@@ -7,7 +7,7 @@ SEPARATOR = " --- "
 
 def print_results(results):
     for res in results:
-        print(str(res[0]) + SEPARATOR + "Rating:" + str(res[2]) + SEPARATOR + str(res[1]))
+        print(str(res[0]) + SEPARATOR + "Rating:" + str(res[2]) + SEPARATOR + str(res[1]) + "\n\n\n")
 
 
 def aggregate(modules):
